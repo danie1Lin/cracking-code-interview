0c48c6b8e4b914b3d5afa294b27fad21e1d42aed
daniellin <s102033114@gapp.nthu.edu.tw>
2021-12-05 00:04:59 +0800

    improvement(3-4): Queue with two stack can be faster

--- ch03/3-4-my-queue-implement-with-two-stack.py
+++ ch03/3-4-my-queue-implement-with-two-stack.py
@@ -9,27 +9,48 @@ class MyQueue:
         self.list.push(value)
 
     def remove(self):
-        return self.remove_many(1)[0]
-
-    def remove_many(self, count):
         while not self.list.is_empty():
             self.tmp_list.push(self.list.pop())
-        res = []
-        for i in range(count):
-            res.append(self.tmp_list.pop())
+        res = self.tmp_list.pop()
         while not self.tmp_list.is_empty():
             self.list.push(self.tmp_list.pop())
         return res
 
+class MyQueueV2:
+    """
+    v2 改進版
+    如果連續 remove 不會倒過去再倒回來
+    """
+    def __init__(self) -> None:
+        self.list =  Stack()
+        self.tmp_list = Stack()
+
+    def add(self, value):
+        self.list.push(value)
+
+    def remove(self):
+        """
+        倒過去就不需要再倒回來了，因為原本的 list push也沒有影響
+        """
+        if self.tmp_list.is_empty():
+            while not self.list.is_empty():
+                self.tmp_list.push(self.list.pop())
+        return self.tmp_list.pop()
+
 if __name__ == '__main__':
-    q = MyQueue()
-    q.add(1)
-    q.add(2)
-    q.add(3)
-    assert q.remove() == 1
-    assert q.remove() == 2
-    assert q.remove() == 3 
-    q.add(1)
-    q.add(2)
-    q.add(3)
-    assert q.remove_many(3) == [1,2,3]
+    q1 = MyQueue()
+    def test(q):
+        q.add(1)
+        q.add(2)
+        q.add(3)
+        assert q.remove() == 1
+        assert q.remove() == 2
+        q.add(4)
+        assert q.remove() == 3 
+        q.add(5)
+        q.add(6)
+        assert q.remove() == 4
+
+
+    test(q1)
+    test(MyQueueV2())
