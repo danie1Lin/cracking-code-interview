f53bc204f9f1172809655c31ccdc6f4a36e87323
daniellin <s102033114@gapp.nthu.edu.tw>
2021-12-08 14:08:43 +0800

    feat: solve ch04 P3

--- ch04/p2_minimum_height_binary_search_tree.py
+++ ch04/p2_minimum_height_binary_search_tree.py
@@ -3,18 +3,15 @@ import unittest
 
 
 # 建立最小高度 -> 平衡的樹 -> 可以從中位數當跟節點
-
-
-
-def BuildMinimumTree(arr: []) -> Optional[BinaryTreeNode]:
+def build_minimum_tree(arr: []) -> Optional[BinaryTreeNode]:
     if len(arr) == 0:
         return None
     mid = int(len(arr) / 2)
     root = BinaryTreeNode(arr[mid])
     if len(arr) == 1:
         return root
-    root.left = BuildMinimumTree(arr[0:mid])
-    root.right = BuildMinimumTree(arr[mid+1:])
+    root.left = build_minimum_tree(arr[0:mid])
+    root.right = build_minimum_tree(arr[mid+1:])
     """
     python 的 range index 超過不會怎麼樣, 其他語言要注意一下
     >>> [0,1,2][4:]
@@ -30,7 +27,7 @@ class TestBuildMinimumTree(unittest.TestCase):
     ]
     def test_build_minimum_tree(self):
         for case in self.TestCases:
-            for i in BuildMinimumTree(case).in_order():
+            for i in build_minimum_tree(case).in_order():
                 print(f'{i} ', end='')
             print()
 
---
+++ ch04/p3_binary_tree_layer_list.py
@@ -0,0 +1,58 @@
+from typing import Deque, Optional, Tuple
+from .tree import BinaryTreeNode
+from .p2_minimum_height_binary_search_tree import build_minimum_tree
+import unittest
+
+def layer_list_of_tree(root: Optional[BinaryTreeNode]) -> [[int]]:
+    q: Deque[Tuple[Optional[BinaryTreeNode], int]] = Deque()
+    q.append((root, 0))
+    result = []
+    while len(q) != 0:
+        node, layer = q.popleft()
+        if node:
+            if layer > len(result) - 1:
+                for i in range(len(result)-1, layer+1):
+                    result.append([])
+            if not result[layer]:
+                result[layer] = []
+            result[layer].append(node.value)
+        else:
+            continue
+        if node.left:
+            q.append((node.left, layer + 1))
+        if node.right:
+            q.append((node.right, layer + 1))
+    return result
+
+# Noticed that list in python is mutable its id wil not change even after expanded.
+# in other language like: 
+# Java ArrayList
+# Golang *[]T
+def layer_list_of_tree_dfs(root: Optional[BinaryTreeNode], layer_list: list[list[int]], layer=0 ): 
+    if not root:
+        return [[]]
+    if layer_list is None:
+        raise ValueError('writable layer_list not given')
+    while len(layer_list) <= layer:
+        layer_list.append([])
+    layer_list[layer].append(root.value)
+    layer_list_of_tree_dfs(root.left, layer_list, layer + 1)
+    layer_list_of_tree_dfs(root.right, layer_list, layer + 1)
+    return layer_list
+    
+
+class TestLayerListOfTree(unittest.TestCase):
+    def test_layer_list_of_tree(self):
+        tree = build_minimum_tree(list(range(15)))
+        li = layer_list_of_tree(tree)
+        print(li)
+    def test_layer_list_of_tree_bfs(self):
+        tree = build_minimum_tree(list(range(15)))
+        layer_list = []
+        layer_list_of_tree_dfs(tree, layer_list)
+        print(layer_list)
+        assert layer_list == layer_list_of_tree(tree)
+
+        
+if __name__ == '__main__':
+    unittest.main()
