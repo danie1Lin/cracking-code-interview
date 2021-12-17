510b02724dee5e3242ea32e4ec0ddb01fc96bd30
daniellin <s102033114@gapp.nthu.edu.tw>
2021-12-17 21:11:08 +0800

    fix: ch01 p2 use default dict instead of set

--- ch01/p2_check_permutation.py
+++ ch01/p2_check_permutation.py
@@ -1,25 +1,32 @@
 # permutation means a different/same sequence of another sequence
+from typing import DefaultDict
 import unittest
+
+# should ask interviewer:
+# 1. does space count in?
+# 2. does different cases means different? like: God and dog
 
 def check_permutation(a: str, b: str) -> bool:
     if len(a) != len(b):
         return False
-    word_set = set()
+    # should not use set, because words can be duplicated.
+    word_set = DefaultDict(lambda: 0)
     for c in a:
-        word_set.add(c)
-    try:
-        for c in b:
-            word_set.remove(c)
-    except KeyError:
-        return False
-    if len(word_set) != 0:
-        return False
+        word_set[c] += 1
+
+    for c in b:
+        word_set[c] -= 1
+
+    for count in word_set.values():
+        if count != 0:
+            return False
     return True
 
 
 class TestCheckPermutation(unittest.TestCase):
     def test(self):
         self.assertTrue(check_permutation("1234", "4321"))
+        self.assertTrue(check_permutation("11234", "43211"))
         self.assertFalse(check_permutation("1234", "5321"))
         self.assertFalse(check_permutation("1234", "321"))
 
