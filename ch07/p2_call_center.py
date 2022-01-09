8f83fa68c147c971b5068dc492d29a5e857d8ce5
daniellin <s102033114@gapp.nthu.edu.tw>
2022-01-09 15:08:57 +0800

    feat: refector ch07 p2

--- ch07/p2_call_center.py
+++ ch07/p2_call_center.py
@@ -1,4 +1,5 @@
 from types import SimpleNamespace
+from typing import DefaultDict, Deque, Dict
 import unittest
 from enum import Enum
 
@@ -12,26 +13,26 @@ class Hierarchy(Enum):
 class Employee:
     def __init__(self, name, hierarchy=Hierarchy.Respondent, direct_reports=None) -> None:
         self.name = name
+        self.supervisor = None
         if direct_reports == None:
             self.direct_reports = []
         else:
             self.direct_reports = direct_reports
         self.is_free = True
         self.hierarchy = hierarchy
+        for direct_report in self.direct_reports:
+            direct_report.supervisor = self
 
     def answer_the_call(self, problem) -> bool:
-        for report in self.direct_reports:
-            if report.answer_the_call(problem):
-                return True
         if self.is_free:
             can_solve = self.hierarchy.can_solve_problem(problem.level)
             if can_solve:
                 problem.assignee = self
                 problem.solved = True
                 print(f'{self} answer the call')
-            return can_solve
-        else:
-            return False
+                return True
+        if self.supervisor:
+            return self.supervisor.answer_the_call(problem)
 
     def __str__(self) -> str:
         return f'{self.hierarchy.name} {self.name}'
@@ -43,10 +44,22 @@ class Problem(SimpleNamespace):
 
 class CallCenter:
     def __init__(self, director) -> None:
-        self.director = director
+        self.employee_level_map = DefaultDict(lambda: Deque()) 
+        temp = Deque()
+        temp.append(director)
+        while len(temp) != 0:
+            employee = temp.pop()
+            self.employee_level_map[employee.hierarchy.value].append(employee)
+            for report in employee.direct_reports:
+                temp.append(report)
 
     def dispatchCall(self, problem: Problem) -> bool:
-        return self.director.answer_the_call(problem)
+        return self.get_assignee_by_problem(problem).answer_the_call(problem)
+
+    def get_assignee_by_problem(self, problem) -> Employee:
+        employee = self.employee_level_map[problem.level].popleft()
+        self.employee_level_map[problem.level].append(employee)
+        return employee
 
 class TestCallCenter(unittest.TestCase):
     def setUp(self) -> None:
@@ -57,15 +70,15 @@ class TestCallCenter(unittest.TestCase):
         return super().setUp()
 
     def test_find_respondent_is_free(self):
-        problem = Problem(level=0, solved=False, assignee=None)
+        problem = Problem(level=1, solved=False, assignee=None)
         self.assertTrue(self.call_center.dispatchCall(problem))
 
     def test_respondent_can_not_handle(self):
-        problem = Problem(level=0, Solved=False, Assignee=None)
+        problem = Problem(level=2, Solved=False, Assignee=None)
         self.assertTrue(self.call_center.dispatchCall(problem))
 
     def test_respondent_are_not_available(self):
-        problem = Problem(level=0, Solved=False, Assignee=None)
+        problem = Problem(level=1, Solved=False, Assignee=None)
         for respondent in self.respondents:
             respondent.is_free = False
         self.assertTrue(self.call_center.dispatchCall(problem))
@@ -73,7 +86,7 @@ class TestCallCenter(unittest.TestCase):
         self.assertEqual(problem.assignee.hierarchy, Hierarchy.Manager)
 
     def test_manager_is_not_free(self):
-        problem = Problem(level=0, Solved=False, Assignee=None)
+        problem = Problem(level=1, Solved=False, Assignee=None)
         for respondent in self.respondents:
             respondent.is_free = False
         for manager in self.managers:
