07178083b07dd5692714e405c9e49fc3d6985b63
daniellin <s102033114@gapp.nthu.edu.tw>
2022-02-02 00:58:01 +0800

    feat(ch08): slight optimize p10

--- ch08/p10_paint_fill.py
+++ ch08/p10_paint_fill.py
@@ -8,15 +8,11 @@ def paint_fill(canvas: list[list[int]], color: int, r: int, c: int):
     queue.append((r, c))
     while queue:
         r, c = queue.pop()
-        if (r, c) in visited:
-            continue
         visited.add((r, c))
         canvas[r][c] = color
         for next_r in range(max(0, r - 1), min(len(canvas)-1, r+1) + 1):
             for next_c in range(max(0, c - 1), min(len(canvas[next_r])-1, c+1) + 1):
-                if next_c == c and next_r == r:
-                    continue
-                if canvas[next_r][next_c] == origin:
+                if canvas[next_r][next_c] == origin and (next_r, next_c) not in visited:
                     queue.append((next_r, next_c))
 
 
@@ -26,7 +22,7 @@ def test_paint_fill():
         [0, 1, 1, 1, 0],
         [0, 1, 0, 1, 1],
         [0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 1]
     ]
 
     paint_fill(canvas, 2, 0, 0)
@@ -36,7 +32,7 @@ def test_paint_fill():
         [2, 1, 1, 1, 2],
         [2, 1, 0, 1, 1],
         [2, 1, 1, 1, 2],
-        [2, 2, 2, 2, 2]
+        [2, 2, 2, 2, 1]
     ]
 
     assert expect == canvas
