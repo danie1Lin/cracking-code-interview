a3925f3d6f179216c097b0decd88bd2063e62db1
daniellin <s102033114@gapp.nthu.edu.tw>
2022-02-05 23:29:40 +0800

    feat(ch08): p14 dp solution

--- ch08/p14_boolean_evaluation.py
+++ ch08/p14_boolean_evaluation.py
@@ -2,7 +2,45 @@
 # 對於這種題型來說遞迴的方式不太一樣
 # 1 ^ 0 | 0 | 1
 # 1 ^ (...) + (1 ^ 0) | (...) + (1^0|0) ^ (...)
+from typing import DefaultDict
+
+
 DEBUG = False
+
+
+def count_evaluate_dp(eq: str, result: bool) -> int:
+    # state[offset][True/False] = ways
+    state = DefaultDict(lambda: DefaultDict(lambda: [0, 0]))
+    for i in range(0, len(eq), 2):
+        state[eq[i]][i][int(eq[i])] = 1
+    for l in range(3, len(eq)+1, 2):
+        for i in range(0, len(eq) - l+1, 2):
+            tmp_eq = eq[i:i+l]
+            for op_idx in range(1, len(tmp_eq), 2):
+                op = tmp_eq[op_idx]
+                left = tmp_eq[:op_idx]
+                right = tmp_eq[op_idx+1:]
+                left_ways = state[left][i]
+                right_ways = state[right][i+op_idx+1]
+                for left_value, left_way_count in enumerate(left_ways):
+                    for right_value, right_way_count in enumerate(right_ways):
+                        match op:
+                            case "^":
+                                state[tmp_eq][i][int(bool(left_value) ^ bool(
+                                    right_value))] += left_way_count * right_way_count
+                            case "|":
+                                state[tmp_eq][i][int(bool(left_value) | bool(
+                                    right_value))] += left_way_count * right_way_count
+                            case "&":
+                                state[tmp_eq][i][int(bool(left_value) & bool(
+                                    right_value))] += left_way_count * right_way_count
+                            case _:
+                                raise RuntimeError(
+                                    f"{tmp_eq}: from:{i} length:{l} op_idx: {op_idx}")
+    way = 0
+    for ways in state[eq].values():
+        way += ways[result]
+    return way
 
 
 def count_evaluate(eq: str, result: bool, deep=0) -> int:
@@ -64,6 +102,7 @@ def get_right_value(left: bool, result: bool, op: str) -> list[bool]:
 
 
 def test_count_evaluate():
-    assert 10 == count_evaluate("0&0&0&1^1|0", True)
-    assert 2 == count_evaluate("1^0|0|1", False)
-    assert 547 == count_evaluate("1^1^0^0&0|1&1&1^1", True)
+    for f in [count_evaluate_dp,  count_evaluate]:
+        assert 10 == f("0&0&0&1^1|0", True)
+        assert 2 == f("1^0|0|1", False)
+        assert 547 == f("1^1^0^0&0|1&1&1^1", True)
